2e1d6cf63b533b753afc891ce2435cb95eba3d0e
Jan-Christian Foeh <jan@programmanstalt.de>
2015-11-05 17:18:00 +0100

    Adds an optional handler for mocking endpoints with static files

---
+++ lib/apidiesel/handlers/mock_response.rb
@@ -0,0 +1,53 @@
+module Apidiesel
+  module Handlers
+    module MockResponse
+      class RequestHandler
+        def run(request, api_config)
+          return request unless request.action.respond_to?(:mock_response)
+
+          file_name = request.action.mock_response[:file]
+          parser    = request.action.mock_response[:parser]
+          file      = File.read(file_name)
+
+          request.response_body = if parser
+            parser.call(file)
+
+          elsif file_name.ends_with?('.json')
+            JSON.parse(file)
+
+          elsif file_name.ends_with?('.xml')
+            Hash.from_xml(file)
+
+          else
+            file
+          end
+
+          request
+        end
+      end
+
+      module ActionExtension
+        extend ActiveSupport::Concern
+
+        class_methods do
+          def mock_response!(file:, &block)
+            @mock_response = {
+              file: file,
+              parser: block
+            }
+          end
+
+          def mock_response
+            @mock_response
+          end
+        end
+
+        def mock_response
+          self.class.mock_response
+        end
+      end
+    end
+  end
+end
+
+Apidiesel::Action.send(:include, Apidiesel::Handlers::MockResponse::ActionExtension)
