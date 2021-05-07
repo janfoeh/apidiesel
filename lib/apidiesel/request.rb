fa85175dd32f3adc649f0ce8369fd157cbd65753
Jan-Christian Foeh <jan@programmanstalt.de>
2021-05-07 12:26:07 +0200

    improves documentation on `Request`

--- lib/apidiesel/request.rb
+++ lib/apidiesel/request.rb
@@ -1,15 +1,44 @@
 # frozen_string_literal: true
 
 module Apidiesel
-
   # Wrapper for API requests
   class Request
-    attr_accessor :endpoint, :endpoint_arguments, :parameters, :url, :response_body, :http_request, :http_response, :metadata, :result
+    # @return [Apidiesel::Endpoint] the endpoint instance which
+    #   built this request
+    attr_accessor :endpoint
+    # @return [Hash] the raw parameters passed by the user into
+    #   the endpoint. Also includes parameters which are not included
+    #   in `parameters`, because they are marked as `submit: false`
+    attr_accessor :endpoint_arguments
+    # @return [Hash] the request parameters to be transmitted, processed
+    #   and filtered by the `expects {}` block validations
+    attr_accessor :parameters
+    # @return [URI] the request URI
+    attr_accessor :url
+    # @return [Object] the response body after all processing by every
+    #   involved response handler (eg., a parsed JSON response). Falls back
+    #   to the raw response body if no processing took place
+    attr_accessor :response_body
+    # @return [HTTPI::Request]
+    attr_accessor :http_request
+    # @return [HTTPI::Response]
+    attr_accessor :http_response
+    # @return [Hash] additional freeform data attached by request and response
+    #   handlers, such as timings
+    attr_accessor :metadata
+    # @return [Object] the final response result, as processed by the endpoints
+    #   `responds_with {}` block
+    attr_accessor :result
 
-    # @param endpoint           [Apidiesel::Endpoint]
-    # @param endpoint_arguments [Hash]
-    # @param parameters         [Hash]
-    # @param metadata           [Hash]
+    # @param endpoint           [Apidiesel::Endpoint] the endpoint instance which
+    #   built this request
+    # @param endpoint_arguments [Hash] the raw parameters passed by the user into
+    #   the endpoint. Also includes parameters which are not included
+    #   in `parameters`, because they are marked as `submit: false`
+    # @param parameters         [Hash] the request parameters to be transmitted, processed
+    #   and filtered by the `expects {}` block validations
+    # @param metadata           [Hash] additional freeform data attached by request and response
+    #   handlers, such as timings
     # @return [self]
     def initialize(endpoint:, endpoint_arguments:, parameters:, metadata: {})
       @endpoint           = endpoint
@@ -18,10 +47,19 @@ module Apidiesel
       @metadata           = metadata
     end
 
+    # The response body after all processing by every involved response handler
+    # (eg., a parsed JSON response). Falls back to the raw response body if no
+    # processing took place
+    #
+    # @return [Object]
     def response_body
       @response_body || http_response.try(:body)
     end
 
+    # Executes the endpoints `responds_with {}` block to create the final `#result`
+    #
+    # @raise [Apidiesel::ResponseError]
+    # @return [void]
     def process_response
       # Reraise ResponseErrors to include ourselves. Not
       # pretty, but I can't think of anything nicer right now
@@ -33,6 +71,7 @@ module Apidiesel
       end
     end
 
+    # @return [String]
     def to_s
       [
         "Apidiesel::Request",
