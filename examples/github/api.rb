cc5ab81616fdfb44f4160e62686d7c6eabdde59c
Jan-Christian Foeh <jan@programmanstalt.de>
2021-08-20 11:22:39 +0200

    refactors the Github example to work with v2

--- examples/github/api.rb
+++ examples/github/api.rb
@@ -2,7 +2,7 @@
 
 require "apidiesel"
 
-Dir[ File.join(__dir__, 'endpoints', '*.rb') ].each do |file|
+Dir[ File.join(__dir__, "endpoints", "**", "*.rb") ].each do |file|
   require file
 end
 
@@ -10,8 +10,6 @@ module Github
   class Api < Apidiesel::Api
     use Apidiesel::Handlers::JSON
 
-    url 'https://api.github.com'
-
-    register_endpoints
+    base_url 'https://api.github.com'
   end
 end
