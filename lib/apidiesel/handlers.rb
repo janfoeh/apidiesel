0857e5a0ff8fa61e51b6939680d94bff0c55c914
Jan-Christian Foeh <jan@programmanstalt.de>
2024-10-27 13:11:46 +0100

    allows handlers to be included selectively
    Now we can mix-and-match, eg. have an endpoints' request processed
    by the form handler and the response processed as JSON.

--- lib/apidiesel/handlers.rb
+++ lib/apidiesel/handlers.rb
@@ -8,13 +8,13 @@ module Apidiesel
     # @param args   [Array<Object>] passed through to the handler
     # @param kargs  [Array<Object>] passed through to the handler
     # @return [void]
-    def use(klass, *args, **kargs, &block)
+    def use(klass, *args, on: :all, **kargs, &block)
       instance =
         klass.new(*args, **kargs, &block)
 
-      config.request_handlers.append(instance) if instance.respond_to?(:handle_request)
-      config.response_handlers.append(instance) if instance.respond_to?(:handle_response)
-      config.exception_handlers.append(instance) if instance.respond_to?(:handle_exception)
+      config.request_handlers.append(instance) if instance.respond_to?(:handle_request) && (on == :all || on == :request)
+      config.response_handlers.append(instance) if instance.respond_to?(:handle_response) && (on == :all || on == :response)
+      config.exception_handlers.append(instance) if instance.respond_to?(:handle_exception) && (on == :all || on == :exception)
     end
   end
 end
